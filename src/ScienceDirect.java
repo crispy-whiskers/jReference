0f1b037ecb6da06ef1adb400b99de8c141571be3
Allen Nguyen <a-nguyen@Allens-MacBook-Pro.local>
2018-05-02 11:58:39 -0400

    Upload Commit

---
+++ src/ScienceDirect.java
@@ -0,0 +1,132 @@
+
+import org.jsoup.select.Elements;
+
+import java.util.regex.*;
+
+
+public class ScienceDirect {
+    public static String[] AuthorAPA(org.jsoup.nodes.Document doc) {
+        String lastnames[] = doc.getElementsByClass("text surname").text().split("\\s+");
+
+        Elements element = doc.getElementsByClass("text given-name"); //parse first and last name classes
+
+        String firstinit[] = element.eachText().toArray(new String[element.size()]);
+        String init = "";
+        Pattern p = Pattern.compile("\\b[a-zA-Z]");
+        Matcher d;
+
+
+        for(int a = 0; a<firstinit.length;a++){     //check for middle initials
+            if(firstinit[a]!=null) {
+                d = p.matcher(firstinit[a]);
+
+                if (d.find()) {
+                    init += firstinit[a].substring(d.start(), d.end());
+                }
+            }
+        }
+
+        String names[] = new String[lastnames.length];
+        String holder[] = init.split("");
+
+        for (int b = 0; b < firstinit.length; b++) { //append first initial to name
+
+                names[b] = lastnames[b] + ", " + holder[b] + ".";
+        }
+        return names;
+    }
+    public static String AuthorMLA(org.jsoup.nodes.Document doc) {
+        String lastnames[] = doc.getElementsByClass("text surname").text().split("\\s+");
+
+        Elements element = doc.getElementsByClass("text given-name"); //parse first and last name classes
+        String firstname[] = element.eachText().toArray(new String[element.size()]);
+        String init = "";
+        Pattern p = Pattern.compile("\\b[a-zA-Z]");
+        Matcher d;
+
+
+        String names = "";
+        String holder[] = init.split("");
+
+        if (lastnames.length == 1)
+            names = lastnames[0] + ", " + firstname[0] + ".";
+        else if(lastnames.length==2)
+            names=lastnames[0] + ", " + firstname[0] + " and "+firstname[1]+" "+lastnames[1];
+        else
+            names=lastnames[0] + ", " + firstname[0] + ", et al.";
+
+        return names;
+    }
+
+
+
+    public static String Date(org.jsoup.nodes.Document doc) {
+        String date = doc.getElementsByClass("size-m").text();
+
+        Pattern datepattern = Pattern.compile("\\d\\d\\d\\d");
+        Matcher m = datepattern.matcher(date); //just parse for teh year
+        if (m.find()) //if found a match
+            return date.substring(m.start(), m.end());
+        else
+            return "Date Not Found";
+
+    }
+
+    public static String[] Volume(org.jsoup.nodes.Document doc) {
+        String vol = doc.getElementsByClass("size-m").text(); //
+        Pattern vi = Pattern.compile("(Volume\\s\\d+,\\s[^Issue])"); //find vol and issue number
+        Pattern vi2 = Pattern.compile("(Volume\\s\\d+,\\sIssue\\s\\d+)");
+        Matcher m = vi.matcher(vol); //first regex to find case issue and volume
+        Matcher n = vi2.matcher(vol);
+        String numberarray[] = new String[2]; //new array to store numbers
+
+        if(m.find()){  //just volume
+
+            vol = vol.substring(m.start(),m.end()-1);
+            Pattern num = Pattern.compile("\\d{1,4}"); //find number
+            Matcher d = num.matcher(vol);
+            d.find();
+            numberarray[0]=vol.substring(d.start(),d.end());
+            numberarray[1]="none";
+
+        }
+        else if(n.find()){ //volume + issue
+
+            vol = vol.substring(n.start(), n.end());
+            Pattern num = Pattern.compile("\\d{1,4}"); //find number
+            Matcher d = num.matcher(vol);
+            for(int count=0;d.find();count++) {
+            numberarray[count]=vol.substring(d.start(),d.end());
+            }
+
+        } else {
+            String response[] = {"Journal Num Not found", "Journal Num Not found"};
+            return response;
+        }
+        return numberarray;
+
+    }
+    public static String Pages(org.jsoup.nodes.Document doc){
+        String text = doc.getElementsByClass("size-m").text();
+        Pattern pg = Pattern.compile("\\d+-\\d+");
+        Matcher m = pg.matcher(text);
+        if(m.find()){
+            return m.group();
+        } else {
+            return "Pages not found";
+        }
+    }
+    public static String Journal(org.jsoup.nodes.Document doc){
+        String element = doc.getElementsByClass("publication-title-link").text();
+        return element; //title is an href element, so just get text
+
+    }
+    public static String DOI(org.jsoup.nodes.Document doc){
+        String element = doc.getElementsByClass("doi").text(); //stored in href element, so just .text()
+        if(element.equals(null))
+            return "DOI not found";
+        return element;
+
+    }
+}
+
---
+++ src/referencePanel.java
@@ -0,0 +1,218 @@
+import net.miginfocom.layout.CC;
+import net.miginfocom.swing.MigLayout;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.util.Date;
+
+
+public class referencePanel extends JPanel {
+
+    private boolean Apa;
+    private boolean Mla;
+    private JTextField url1;
+    private JEditorPane cite;
+
+
+    final String int5gr1ty_2 = "1190de84d01ce12f0c54a01a4752a33e659efad4247b13fd5b4381a63307412f5350491e7f078aa5b24fb3f86f9f722cf9ac2dd2a28a779e467d323d0b1aef605==";
+
+    private referencePanel() {
+
+        JButton about;
+        JLabel instructions, title;   //local fields
+        JButton get;
+
+        setLayout(new MigLayout()); //miglayout is OG
+
+
+        CC cc = new CC(); //component constraint
+        cc.alignX("center").spanX();
+
+        title = new JLabel("<html><h1 style=\"text-align:center\">J-Reference</h1><p><br/>A timesaver for database journal citations.</p></html>", SwingConstants.CENTER);
+        add(title, cc);        //adding panel components
+        url1 = new JTextField(30);
+        //url1.setBackground(new Color(255,228,196));
+        add(url1, cc);
+
+        ButtonGroup group = new ButtonGroup();
+        JRadioButton apa = new JRadioButton("APA 6th Edition");
+        JRadioButton mla = new JRadioButton("MLA 8th Edition");
+        group.add(apa);
+        group.add(mla);
+        apa.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Apa = true;
+                Mla = false;
+            }
+        });
+        mla.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Apa = false;
+                Mla = true;
+            }
+        });
+        add(apa, cc);
+        add(mla, cc);
+
+        get = new JButton("Cite");
+        get.addActionListener(new Listener1());
+        get.setPreferredSize(new Dimension(100, 25)); //button
+        add(get, cc);
+
+        instructions = new JLabel("<html><p style=\"text-align:center\">Paste the url of the journal article above and press Cite when <br> you're ready!</p></html>", SwingConstants.CENTER);
+        add(instructions, cc);
+
+        JPanel subpanel2 = new JPanel();
+        cite = new JEditorPane("text/html", "Your citation will appear here! Everything is already in Times New Roman size 12 :)");
+        cite.setPreferredSize(new Dimension(430, 120));
+        cite.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+        add(cite, cc);
+
+        about = new JButton("About");
+        about.addActionListener(new ActionListener() {
+            int count = 0;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                count++;
+                JOptionPane.showMessageDialog(null, "jAPA-Java Citation generator coded on IntelliJ \n" +
+                        "using JSoup HTML parser and MiGLayout Library.\n\nVersion 1.5\n\nConcept and Coding" +
+                        " by Allen Nguyen", "About me", JOptionPane.INFORMATION_MESSAGE);
+                if (count > 40) {
+                    about.setText("every click ur mum +1 gay");
+                    count = 40;
+                }
+            }
+        });
+
+        add(about, cc);
+
+
+
+    }
+
+    private class Listener1 implements ActionListener { //citation algorithm
+        String names[];
+        String name1;
+        String date;
+        String pages;
+        String journal;
+        String DOI;
+        String title;
+        String vols[];
+        Document doc;
+
+        public void actionPerformed(ActionEvent e) {
+            org.jsoup.Connection.Response response = null;
+            try {
+                response = Jsoup.connect(url1.getText()).timeout(100000) //get error code
+                        .ignoreHttpErrors(true)
+                        .execute();
+                doc = Jsoup.connect(url1.getText()).get(); //get html from database
+
+            } catch (IOException f) {
+                cite.setText("Connection error: could not access site. ");
+            } catch (IllegalArgumentException g) {
+                cite.setText("Whoops, that url isn't valid or supported! Sorry :( ");
+
+            }
+
+
+            if (url1.getText().contains("sciencedirect")) {
+
+
+                if (Apa && !Mla) {
+                    getAPAScienceDirect(doc);
+
+                    String name = "";
+                    if (names.length != 1) {
+                        for (int a = 0; a < names.length; a++) { //todo problem here for 6+ authors: see SD class
+                            if (a == names.length - 1) {
+                                name += "& " + names[a];
+                            } else if (a == 7) {
+                                name += " et al.";
+                                break;
+                            } else if (a < (names.length - 2)) {
+                                name += names[a] + ", ";
+                            }
+                        }
+                    } else {
+                        name = names[0];
+                    }
+
+                    date = "(" + date + "). ";
+
+
+                    if (!vols[1].equals("none"))
+                        cite.setText("<html><p>" + name + " " + date + title + "<i>" + journal + ", " + vols[0] + "</i>(" + vols[1] + "). " + DOI + "</p></html>");
+                    else
+                        cite.setText("<html><p>" + name + " " + date + title + "<i>" + journal + ", " + vols[0] + "</i>. " + DOI + "</p></html>"); //volume to volume(issue)
+                } else if (!Apa && Mla) {
+                    getMLAScienceDirect(doc);
+
+                    Date objDate = new Date();
+                    String strDateFormat = "dd MMM yyyy";
+                    SimpleDateFormat objSDF = new SimpleDateFormat(strDateFormat);
+
+                    String accessed = objSDF.format(objDate);
+                    if (!vols[1].equals("none"))
+                        cite.setText("<html><p>" + name1 + " \"" + title + "\" <i>" + journal + "</i> " + vols[0] + ". (" + date + "): " + pages + ". <i>ScienceDirect.</i> Web. " + accessed + "</p></html>");
+                    else
+                        cite.setText("<html><p>" + name1 + " \"" + title + "\" <i>" + journal + "</i>. " + vols[0] + ". (" + date + "): " + pages + ". <i>ScienceDirect.</i> Web. " + accessed + ". </p></html>");
+
+                }
+            } else if (url1.getText().contains("proquest.com") || url1.getText().contains("jstor.org") || url1.getText().contains("gale.") || url1.getText().contains("accessscience.com")) {
+                cite.setText("I sure as heck don't to code support for that database in. The website literally gives you the entire citation so go get it smh");
+            }
+
+        }
+
+        private void getAPAScienceDirect(Document doc) {
+            int hyphen = (doc.title().lastIndexOf(45));
+            title = doc.title().substring(0, hyphen - 1); //get title, format it
+            title += ". ";
+            names = ScienceDirect.AuthorAPA(doc); //should return Lastname, F.
+            date = ScienceDirect.Date(doc); //return year
+            vols = ScienceDirect.Volume(doc); //return volume(issue)
+            pages = ScienceDirect.Pages(doc); //return pg-pgs
+            journal = ScienceDirect.Journal(doc); //return journaltitle
+            DOI = ScienceDirect.DOI(doc); //return DOI, if there is one
+        }
+
+        private void getMLAScienceDirect(Document doc) {
+            int hyphen = (doc.title().lastIndexOf(45));
+            title = doc.title().substring(0, hyphen - 1); //get title, format it
+            title += ".";
+            name1 = ScienceDirect.AuthorMLA(doc);
+            date = ScienceDirect.Date(doc); //return year
+            vols = ScienceDirect.Volume(doc); //return volume(issue)
+            pages = ScienceDirect.Pages(doc); //return pg-pgs
+            journal = ScienceDirect.Journal(doc); //return journaltitle
+            DOI = ScienceDirect.DOI(doc); //return DOI, if there is one
+        }
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Java Citation Generator");
+        frame.setSize(470, 480);
+        frame.setLocation(400, 100);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setContentPane(new referencePanel());
+        frame.getContentPane().setBackground(new Color(255, 241, 208));
+
+
+        frame.setVisible(true);
+
+    }
+}
+
